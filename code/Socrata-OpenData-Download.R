ed9efbe07a11c0e331b2929d1ec47ca30a2516be
dnchelst <dchelst@cpsm.us>
2018-08-28 17:30:51 -0400

    new commit to update the time on a blog post

--- code/Socrata-OpenData-Download.R
+++ code/Socrata-OpenData-Download.R
@@ -5,3 +5,4 @@ new.orleans.district1 <-
     read.socrata("https://data.nola.gov/resource/ahnh-7f2h.csv?PoliceDistrict=1")
 download.file("https://data.nola.gov/api/views/wgrp-d3ma/rows.csv?accessType=DOWNLOAD", 
               "new_orleans.csv", method="curl")
+los.angeles <- read.socrata("https://data.lacity.org/resource/7fvc-faax.csv")
